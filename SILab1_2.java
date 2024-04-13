109c8dc136cebe855945aea36dd20783a4dcc31d
ma100ma100 <marija.stojanovska@students.finki.ukim.mk>
2024-04-14 01:55:21 +0200

    Fix wrong condition in makeBigStringFrom

--- SILab1_2.java
+++ SILab1_2.java
@@ -6,7 +6,7 @@ public class SILab1_2 {
         StringBuilder sb = new StringBuilder();
         for (String el : list) {
             if (el.length() >= minLength) {
-                sb.append(el);
+                sb.append(el); 
             }
         }
         return sb.toString();
